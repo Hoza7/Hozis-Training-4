c2cbb8a38b23d1d1ce16475140de4220e320d24f
Hoza7 <Hossanaorho@gmail.com>
2024-10-23 12:23:24 +0000

    ~/Hozis-Training-4$

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 # Classes
 
 class Dog:
-  def bark(self):
-    print("woof!")
+    def __init__ (self, name, age):
+      self.name = name
+      self.age = age
+  
+    def bark(self):
+        print("woof!")
 
-roger = Dog()
+roger = Dog("Roger", 8)
 
-print(type(roger))
+print(roger.name)
+print(roger.age)
+
+print(roger.bark())
 
