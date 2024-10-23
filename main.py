607306390859063a49c110b0fc26efb5c187ca04
Hoza7 <Hossanaorho@gmail.com>
2024-10-23 12:29:11 +0000

    ~/Hozis-Training-4$

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # Classes
 
-class Dog:
+class Animal:
+    def walk(self):
+        print("Walking")
+      
+class Dog(Animal):
     def __init__ (self, name, age):
       self.name = name
       self.age = age
@@ -13,5 +17,5 @@ roger = Dog("Roger", 8)
 print(roger.name)
 print(roger.age)
 
-print(roger.bark())
-
+roger.bark()
+roger.walk()
