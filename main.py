4b47850d05585d459eb1779046b8438ecc026210
Hoza7 <Hossanaorho@gmail.com>
2024-10-23 11:52:16 +0000

    ~/Hozis-Training-4$

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# While Loops
+# For Loops
 
-count = 0
-while count < 10:
-  print("The condition is True")
-  count = count + 1
-
-print("After the loop")
+items = [1, 2, 3, 4]
+for item in items:
+    print(item)
 
